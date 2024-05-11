681ae6f10570511dab194fddfab8da2dea778181
Geovane Silva <geovanesilva.nar@hotmail.com>
2024-05-11 11:11:29 -0300

    feat: Update ClientRepository to return ClientEntity or null for findByName method

--- src/modules/client/domain/application/repositories/client.repository.ts
+++ src/modules/client/domain/application/repositories/client.repository.ts
@@ -2,5 +2,5 @@ import { BaseRepository } from "libs/core/src/repositories";
 import { ClientEntity } from "../../enterprise/client.entity";
 
 export abstract class ClientRepository extends BaseRepository<ClientEntity> {
-  abstract findByName(name: string): Promise<Boolean>
+  abstract findByName(name: string): Promise<ClientEntity | null>
 }
