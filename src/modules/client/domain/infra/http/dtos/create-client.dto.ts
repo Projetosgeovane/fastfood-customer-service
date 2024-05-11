dc40443e0864deed91719bbb8861329d580cff8a
Geovane Silva <geovanesilva.nar@hotmail.com>
2024-05-11 11:12:17 -0300

    feat: Add create and edit client DTOs

---
+++ src/modules/client/domain/infra/http/dtos/create-client.dto.ts
@@ -0,0 +1,8 @@
+export class CreateClientDTO {
+  name: string;
+  cpf: string;
+  email: string;
+  password: string;
+  phone: string;
+  status: boolean;
+}
---
+++ src/modules/client/domain/infra/http/dtos/edit-client.dto.ts
@@ -0,0 +1,6 @@
+export class EditClientDTO {
+  name: string;
+  phone: string;
+  password: string;
+  status: boolean;
+}
