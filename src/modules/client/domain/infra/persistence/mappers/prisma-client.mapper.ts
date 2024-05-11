7bef838392c9af60224958659d71bebbcf451713
Geovane Silva <geovanesilva.nar@hotmail.com>
2024-05-11 11:12:31 -0300

    feat: Add PrismaClientMapper for mapping between PrismaClient and ClientEntity

---
+++ src/modules/client/domain/infra/persistence/mappers/prisma-client.mapper.ts
@@ -0,0 +1,41 @@
+import { Prisma, Client as PrismaClient } from "@prisma/client";
+import { ClientEntity } from "../../../enterprise/client.entity";
+import { UniqueEntityID } from "libs/core/src/entities";
+
+export class PrismaClientMapper {
+  static toDomain(raw: PrismaClient): ClientEntity {
+    const client = ClientEntity.instance(
+      {
+        name: raw.name,
+        cpf: raw.cpf,
+        email: raw.email,
+        password: raw.password,
+        phone: raw.phone,
+        status: raw.status,
+        createdAt: raw.createdAt,
+        updatedAt: raw.updatedAt,
+        deletedAt: raw.deletedAt,
+      },
+      new UniqueEntityID(raw.id),
+    );
+
+    return client;
+  }
+
+  static toPrisma(
+    client: ClientEntity,
+  ): Prisma.ClientUncheckedCreateInput {
+    return {
+      id: client.id.toValue(),
+      name: client.name,
+      cpf: client.cpf,
+      email: client.email,
+      password: client.password,
+      phone: client.phone,
+      status: client.status,
+      createdAt: client.createdAt,
+      updatedAt: client.updatedAt,
+      deletedAt: client.deletedAt,
+    };
+  }
+}
---
+++ src/modules/client/domain/infra/persistence/prisma/repositories/prisma-client.repository.impl.ts
@@ -0,0 +1,81 @@
+import { Injectable } from "@nestjs/common";
+import { PaginationParams } from "libs/core/src/repositories";
+import { ClientRepository } from "src/modules/client/domain/application/repositories/client.repository";
+import { ClientEntity } from "src/modules/client/domain/enterprise/client.entity";
+import { PrismaClientMapper } from "../../mappers/prisma-client.mapper";
+import { PrismaService } from "src/common/database/prisma/prisma.service";
+
+@Injectable()
+export class PrismaClientRepositoryImpl implements ClientRepository {
+  private readonly PERPAGE = 20;
+  constructor(private readonly prisma: PrismaService) { }
+
+  async findByName(name: string): Promise<ClientEntity | null> {
+    const client = await this.prisma.client.findFirst({
+      where: {
+        name
+      }
+    })
+
+    if (!client) {
+      return null
+    }
+
+    return PrismaClientMapper.toDomain(client)
+  }
+
+  async create(data: ClientEntity): Promise<void> {
+    const client = PrismaClientMapper.toPrisma(data);
+
+    await this.prisma.client.create({
+      data: {
+        ...client
+      }
+    });
+  }
+
+  async save(body: ClientEntity): Promise<void> {
+    const client = PrismaClientMapper.toPrisma(body);
+
+    await this.prisma.client.update({
+      where: {
+        id: client.id
+      },
+      data: {
+        ...client
+      }
+    })
+  }
+
+  async findManyRecent({ page }: PaginationParams): Promise<ClientEntity[]> {
+    const client = await this.prisma.client.findMany({
+      orderBy: {
+        createdAt: 'desc',
+      },
+      take: this.PERPAGE,
+      skip: (page - 1) * this.PERPAGE
+    })
+
+    return client.map(PrismaClientMapper.toDomain)
+  }
+
+  async findById(id: string): Promise<ClientEntity> {
+    const client = await this.prisma.client.findFirst({
+      where: {
+        id
+      }
+    });
+
+    if (!client) {
+      return null
+    }
+
+    return PrismaClientMapper.toDomain(client)
+  }
+  delete(id: string): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+  softDelete(id: string): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+}
